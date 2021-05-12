7d6970e58e6bf0c741ec937a69ec98f77bf1dcce
Jakub Rychecky <jakub@rychecky.cz>
2021-05-13 00:35:29 +0200

    add Webpack

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
 1. Clone this repository.
 2. Run `npm i` in the folder to install dependencies.
+3. Run `npx webpack -w --mode="development"` in the folder to compile final JS.
+
+## How to compile for production?
+Run `npx webpack --mode="production"`
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+        mode: 'development',
+        entry: './src/index.js',
+        output: {
+            filename: 'csfd-extended.js'
+        },
+    },
+    {
+        mode: 'production',
+        entry: './src/index.js',
+        output: {
+            filename: 'csfd-extended.js'
+        },
+    },
+];
